e165daea5680b50a946bb19c7e694f1178315adf
Amritesh <amriteshk778@gmail.com>
2025-11-05 15:41:00 +0530

    feat: command not found

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <string>
 
-int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+using namespace std;
 
+int main() {
+  // Flush after every cout / cerr
+  cout << unitbuf;
+  cerr << unitbuf;
+
+  string command;
   // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  cout << "$ ";
+  getline(cin, command);
+  cout << command << ": command not found" << endl;
 }
